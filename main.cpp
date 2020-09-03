8d850894741359d999e936354b07ea0017da92d2
Timothy Graupmann <tim.graupmann@razer.com>
2020-09-03 15:59:33 -0700

    Added auto changing mode on connect

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS true
+
+
+#define CURL_STATICLIB
+#include <curl.h>
+
+#include <iostream>
 #include <stdio.h>
 #include <string>
 #include <WinSock.h>
+
 
 using namespace std;
 
@@ -60,6 +68,48 @@ string GetGoProWebcam()
     return "";
 }
 
+size_t CurlWrite_CallbackFunc_StdString(void* contents, size_t size, size_t nmemb, string* s)
+{
+    size_t newLength = size * nmemb;
+    try
+    {
+        s->append((char*)contents, newLength);
+    }
+    catch (std::bad_alloc&)
+    {
+        //handle memory problem
+        return 0;
+    }
+    return newLength;
+}
+
+void RequestGoProWebcamEndpoint(const string ip, const string path)
+{
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    auto curl = curl_easy_init();
+    if (curl) {
+        string url = "http://";
+        url += ip;
+        url += path;
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+
+        std::string response_string;
+        std::string header_string;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
+
+        curl_easy_perform(curl);
+        cout << "Request: " << path << " Response: ===" << endl << response_string << endl;
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+        curl = NULL;
+    }
+}
+
 // ref - https://www.codeguru.com/csharp/csharp/cs_network/article.php/c6045/Obtain-all-IP-addresses-of-local-machine.htm
 int main(int argc, char* argv[])
 {
@@ -77,6 +127,14 @@ int main(int argc, char* argv[])
             else
             {
                 printf("GoPro WebCam %s [Connected]\r\n", strGoProIP.c_str());
+
+                //restart service requires admin
+                system("net stop \"GoPro Webcam Service\"");
+                system("net start \"GoPro Webcam Service\"");
+                
+                RequestGoProWebcamEndpoint(strGoProIP, "/gp/gpWebCam/STOP"); //STOP
+                RequestGoProWebcamEndpoint(strGoProIP, "/gp/gpControl/setting/43/6"); //MODE
+                RequestGoProWebcamEndpoint(strGoProIP, "/gp/gpWebcam/START"); //START
             }
         }
         Sleep(1000);
@@ -84,4 +142,18 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+#ifdef _DEBUG
+#pragma comment( lib, "msvcrtd" )
+#else
+#pragma comment( lib, "msvcrt" )
+#endif
+#pragma comment( lib, "Wldap32" )
+#pragma comment( lib, "Ws2_32" )
+#pragma comment (lib, "crypt32")
+#pragma comment( lib, "libcurl" )
+#pragma comment( lib, "libssh2" )
+#pragma comment( lib, "libcrypto" )
+#pragma comment( lib, "libssl" )
+#pragma comment( lib, "zlibstat" )
+#pragma comment (lib, "jsoncpp")
 #pragma comment(lib, "wsock32.lib")
